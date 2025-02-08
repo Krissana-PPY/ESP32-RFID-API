138043ea3a09735b84ae2c731df48accd20a6c2d
Krissana Pumpayom <krissanapu@kkumail.com>
2025-02-09 05:52:56 +0700

    Websocket

--- ESP32-RFID-Webserver/ESP32-RFID-Webserver.ino
+++ ESP32-RFID-Webserver/ESP32-RFID-Webserver.ino
@@ -4,26 +4,22 @@
 #include <SPIFFS.h>
 #include <WiFi.h>
 #include <WebServer.h>
+#include <WebSocketsServer.h>
 
 #define SS_PIN  5 //SDA
 #define RST_PIN 21 //RST
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
-const char* ssid = "iPhone";
-const char* password = "0951388070z";
+const char* ssid = "ESP32-Access-Point";
+const char* password = "123456789";
 
 WebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
 
-void handleRoot() {
-  File file = SPIFFS.open("/index.html", "r");
-  if (!file) {
-    server.send(404, "text/plain", "File not found");
-    return;
-  }
-  server.streamFile(file, "text/html");
-  file.close();
-}
+void handleRoot();
+void handleIndex();
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length);
 
 void setup() {
   Serial.begin(115200);  // Initialize serial communications
@@ -37,22 +33,25 @@ void setup() {
   }
   Serial.println("SPIFFS initialized.");
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");
+  // Configure ESP32 as an access point
+  WiFi.softAP(ssid, password);
+  Serial.println("Access Point started");
   Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.softAPIP());
 
-  server.on("/", handleRoot);
+  //server.on("/", handleRoot);
+  server.on("/", handleIndex);
+  //server.on("/index.html", handleIndex);
   server.begin();
   Serial.println("HTTP server started");
+
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
 }
 
 void loop() {
   server.handleClient();
+  webSocket.loop();
 
   // Look for new cards
   if (!mfrc522.PICC_IsNewCardPresent()) {
@@ -76,13 +75,14 @@ void loop() {
 
   // Check if the UID is known
   bool uidFound = false;
+  String owner = "";
   File file = SPIFFS.open("/UIDs.csv");
   if (file) {
     while (file.available()) {
       String line = file.readStringUntil('\n');
       int commaIndex = line.indexOf(',');
       String uid = line.substring(0, commaIndex);
-      String owner = line.substring(commaIndex + 1);
+      owner = line.substring(commaIndex + 1);
 
       if (content.equals(uid)) {
         Serial.print("Hello ");
@@ -97,17 +97,108 @@ void loop() {
     Serial.println("Failed to open UIDs.csv");
   }
 
-  // If UID is not found, add it to the file
-  if (!uidFound) {
-    Serial.println("Unknown UID. Adding to UIDs.csv");
-    file = SPIFFS.open("/UIDs.csv", FILE_APPEND);
-    if (file) {
-      file.print(content);
-      file.println(",Unknown");
-      file.close();
-      Serial.println("UID added to UIDs.csv");
-    } else {
-      Serial.println("Failed to open UIDs.csv for appending");
+  // Send UID and owner information to the website
+  if (uidFound) {
+    webSocket.broadcastTXT("KNOWN:" + content + ":" + owner);
+  } else {
+    webSocket.broadcastTXT("UNKNOWN:" + content);
+    Serial.print("UID tag (in red): ");
+    Serial.println(content);
+  }
+}
+
+void handleRoot() {
+  //File file = SPIFFS.open("/login.html", "r");
+  File file = SPIFFS.open("/index.html", "r");
+  if (!file) {
+    server.send(404, "text/plain", "File not found");
+    return;
+  }
+  server.streamFile(file, "text/html");
+  file.close();
+}
+
+void handleIndex() {
+  File file = SPIFFS.open("/index.html", "r");
+  if (!file) {
+    server.send(404, "text/plain", "File not found");
+    return;
+  }
+  server.streamFile(file, "text/html");
+  file.close();
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  if (type == WStype_TEXT) {
+    String message = String((char *)payload);
+    int colonIndex1 = message.indexOf(':');
+    int colonIndex2 = message.indexOf(':', colonIndex1 + 1);
+    String action = message.substring(0, colonIndex1);
+    String uid = message.substring(colonIndex1 + 1, colonIndex2);
+    String owner = message.substring(colonIndex2 + 1);
+
+    if (action == "ADD") {
+      bool uidExists = false;
+      File file = SPIFFS.open("/UIDs.csv", "r");
+      if (file) {
+        while (file.available()) {
+          String line = file.readStringUntil('\n');
+          if (line.startsWith(uid + ",")) {
+            uidExists = true;
+            break;
+          }
+        }
+        file.close();
+      }
+
+      if (uidExists) {
+        webSocket.sendTXT(num, "UID already exists");
+        Serial.println("UID already exists");
+      } else {
+        file = SPIFFS.open("/UIDs.csv", FILE_APPEND);
+        if (file) {
+          file.print(uid);
+          file.print(",");
+          file.println(owner);
+          file.close();
+          webSocket.sendTXT(num, "Successfully registered");
+          Serial.println("UID and owner added to UIDs.csv");
+        } else {
+          Serial.println("Failed to open UIDs.csv for appending");
+        }
+      }
+    } else if (action == "DELETE") {
+      File file = SPIFFS.open("/UIDs.csv", "r");
+      if (file) {
+        String temp = "";
+        bool uidFound = false;
+        while (file.available()) {
+          String line = file.readStringUntil('\n');
+          if (!line.startsWith(uid + ",")) {
+            temp += line + "\n";
+          } else {
+            uidFound = true;
+          }
+        }
+        file.close();
+
+        if (uidFound) {
+          file = SPIFFS.open("/UIDs.csv", "w");
+          if (file) {
+            file.print(temp);
+            file.close();
+            webSocket.sendTXT(num, "User successfully deleted");
+            Serial.println("UID deleted from UIDs.csv");
+          } else {
+            Serial.println("Failed to open UIDs.csv for writing");
+          }
+        } else {
+          webSocket.sendTXT(num, "UID not found");
+          Serial.println("UID not found");
+        }
+      } else {
+        Serial.println("Failed to open UIDs.csv");
+      }
     }
   }
 }
